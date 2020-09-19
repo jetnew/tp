d84e034157508c8103ba8ad9ac87ae62152ac825
jetnew <notesjet@gmail.com>
2020-09-20 00:09:59 +0800

    Add Remark command

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,7 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /** Constructor for remark.*/
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
